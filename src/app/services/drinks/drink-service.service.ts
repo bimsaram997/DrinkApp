75331c17a4a5abcfedde41b8c095cbe7bbc903ea
Bimsara <hirushanbim123@gmail.com>
2024-01-31 14:39:05 +0200

    Initial commit Project structure addedd

---
+++ src/app/services/drinks/drink-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DrinkServiceService {
+  myUrl = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+  getAlcoholicDrinks(): Observable<any> {
+    return this.http.get(`${this.myUrl}/json/v1/1/filter.php?a=Alcoholic`);
+  }
+}
